23982789a6be25bdae0da396b6032dacf9b6abf9
betul.kilincaslan <betul.kilincaslan@inveon.com>
2022-07-13 23:14:51 +0300

    Added product route, get all products function.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDBHandler from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,5 +11,8 @@ const app = express();
 
 app.use(express.json());
 
+// Routes
+app.use("/api/products", productRoutes);
+
 const PORT = process.env.PORT;
-app.listen(3000, () => console.log(`Listening on ${PORT}`));
+app.listen(5000, () => console.log(`Listening on ${PORT}`));
